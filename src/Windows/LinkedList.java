9ec1cee52c5f9f9c6986417e3a1f218aa192cc86
SKM6607 <jksrikoushik@gmail.com>
2025-09-09 19:38:16 +0530

    add: changes in functions

--- src/Windows/LinkedList.java
+++ src/Windows/LinkedList.java
@@ -2,6 +2,8 @@ package Windows;
 import javax.swing.*;
 import java.awt.*;
 import MyShapes.MyArrow;
+import org.jetbrains.annotations.NotNull;
+
 public class LinkedList extends JPanel {
     private static final class Node{
         private int data;
@@ -14,28 +16,61 @@ public class LinkedList extends JPanel {
         }
         public Node next;
     }
-    private Node head;
-    private MyArrow arrow;
+    private final MyArrow arrow;
     private static final int width=Toolkit.getDefaultToolkit().getScreenSize().width;
     private static final int height=Toolkit.getDefaultToolkit().getScreenSize().height;
     private final int nodeLength=150,nodeHeight=100;
     private final int SPACING=25;
     private final JButton[] button=new JButton[2];
     public LinkedList(){
-        //setBlock
+            GridLayout layout=new GridLayout(2,1);
+            JPanel panel=new JPanel();       //setBlock
+            JTextField textField = appendTextInput();
+            JButton append=new JButton("Append");
+            JButton pop=new JButton("Pop");
+            JPanel sidePanel=new JPanel();
+            GridLayout layoutSide=new GridLayout(1,2);
+        sidePanel.setLayout(layoutSide);
+        sidePanel.add(textField);
+        sidePanel.add(append);
+        panel.setLayout(layout);
+        panel.add(sidePanel);
+        panel.add(pop);
+        panel.setPreferredSize(new Dimension(300,75));
+        add(panel,BorderLayout.EAST);
         {
             button[0]=new JButton("Append");
             button[1]=new JButton("Delete");
+            arrow=new MyArrow(nodeLength/2,20);
         }
         repaint();
         setBackground(Color.BLACK);
     }
+
+    private static @NotNull JTextField appendTextInput() {
+        JTextField textField=new JTextField();
+        textField.setToolTipText("Enter the value for the next node(Integer): ");
+        textField.setInputVerifier(new InputVerifier() {
+            @Override
+            public boolean verify(JComponent input) {
+                try {
+                    Integer.parseInt(((JTextField)input).getText());
+                    return true;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+        });
+        textField.setFont(new Font(Font.SANS_SERIF,Font.BOLD,15));
+        return textField;
+    }
+
     @Override
     protected void paintComponent(Graphics g1){
         super.paintComponent(g1);
         Graphics2D g=(Graphics2D) g1;
         drawGrid(g);
-        drawNode(g,new Node(0,width/2,height/2));
+        drawNode(g,new Node(0,width/2-nodeLength/2,height/2-nodeHeight));
     }
     void drawGrid(Graphics2D g){
         g.setStroke(new BasicStroke(1f));
@@ -50,14 +85,14 @@ public class LinkedList extends JPanel {
         int x=node.x;
         int y=node.y;
         int value=node.data;
-        arrow=new MyArrow(nodeLength/2,2);
         g.setColor(Color.WHITE);
         g.fillRect(x,y,nodeLength,nodeHeight);
         g.setStroke(new BasicStroke(4f));
         g.setColor(Color.BLACK);
-        g.drawLine((x+nodeLength)/2,y,(x+nodeLength)/2,y+nodeHeight);
+        g.drawLine(x+nodeLength/2,y,x+nodeLength/2,y+nodeHeight);
+        g.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
         g.drawString(String.valueOf(value),(x+nodeLength)/4,(y+nodeLength)/2);
-        arrow.draw(g,x+nodeLength,(y+nodeHeight)/2,Color.WHITE);
+        arrow.draw(g,x+nodeLength,y+nodeHeight/2,Color.YELLOW);
     }
 
     public static void main(String[] args) {
